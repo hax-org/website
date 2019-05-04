58244d6775cca98def0307e5e1a6b2ebbe07bdce
Olivia <oliviachang29@gmail.com>
2019-05-04 16:08:54 -0700

    add about us page

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,10 @@
+var readmore_enabled = false;
+var lazyLoad_enabled = false;
+var lazyLoadInstance;
+
+function currentWindow() {
+	return window.location.pathname.split('/')[1]
+}
 function initJS() {
 	// add current
 	$('nav li a').filter(function(){
@@ -8,12 +15,24 @@ function initJS() {
 	  return this.href !== location.href;
 	}).removeClass('active');
 
-  $('.readmore').readmore({
-  	speed: 100,
-  	collapsedHeight: 100,
-  	moreLink: '<a class="readmore-link">Read more</a>',
-	lessLink: '<a class="readmore-link">Read less</a>'
-  });
+	if (!readmore_enabled &&  currentWindow() == 'our-team') {
+	  $('.readmore').readmore({
+	  	speed: 100,
+	  	collapsedHeight: 100,
+	  	moreLink: '<a class="readmore-link">Read more</a>',
+			lessLink: '<a class="readmore-link">Read less</a>',
+			afterToggle: function(trigger, element, expanded) {
+        if(! expanded) { // The "Close" link was clicked
+          $('html, body').animate({scrollTop: element.offset().top}, {duration: 100});
+        }
+      }
+	  });
+	  readmore_enabled = true
+	}
+	if (!lazyLoad_enabled) {
+		lazyLoadInstance = new LazyLoad({});
+		lazyLoad_enabled = false
+	}
 }
 
 $(function() {
