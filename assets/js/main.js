5425aaa3e0e8b7c1f995769f22c88c9f8f3e4746
Olivia <oliviachang29@gmail.com>
2019-10-20 11:03:36 -0700

    Highlight nav links correctly and add ribbon

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,11 +3,11 @@
 function markNavLinks() {
 	// add current
 	$('nav li a').filter(function(){
-		return this.href === location.href;
+		return this.href.split("/")[3] === location.href.split("/")[3];
 	}).addClass('active');
 	// remove previous
 	$('nav li a').filter(function(){
-	  return this.href !== location.href;
+	  return this.href.split("/")[3] !== location.href.split("/")[3];
 	}).removeClass('active');
 }
 
